6196679dc1ea676919fd3db4c10d025c43f4636e
Sohyla Gomaa <sohylaelqourashi@gmail.com>
2026-05-10 17:06:24 +0300

    add vue port for cors config

--- config/cors.php
+++ config/cors.php
@@ -25,6 +25,7 @@ return [
         'http://localhost:3000',
         'http://127.0.0.1:5173',
         'http://127.0.0.1:3000',
+        'http://localhost:8080',
     ],
 
     'allowed_origins_patterns' => [],
